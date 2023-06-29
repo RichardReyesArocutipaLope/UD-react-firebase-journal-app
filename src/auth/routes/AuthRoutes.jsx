1d56906014bccf19583c7852fe932ff83cd8537e
Richard Arocutipa Lope <richard.arocutipa@sfact.pe>
2023-06-29 15:49:13 -0500

    Add structure and design

--- src/auth/routes/AuthRoutes.jsx
+++ src/auth/routes/AuthRoutes.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
-import { RegisterPage } from '../pages'
+import { LoginPage, RegisterPage } from '../pages'
 
 export const AuthRoutes = () => {
     return (
