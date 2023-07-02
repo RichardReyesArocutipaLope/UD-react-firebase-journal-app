96f843dc4b043b99cfda4e3118eafebb91097276
Richard Arocutipa Lope <richardreyesarocutipalope@gmail.com>
2023-07-01 19:42:34 -0500

    Add create, upload and display notes

--- src/journal/components/SideBar.jsx
+++ src/journal/components/SideBar.jsx
@@ -1,11 +1,12 @@
 import { TurnedInNot } from "@mui/icons-material"
 import { Box, Divider, Drawer, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from "@mui/material"
 import { useSelector } from "react-redux"
+import { SidebarItem } from "./SidebarItem"
 
 export const SideBar = ({ drawerWidth = 240 }) => {
 
     const { displayName } = useSelector(state => state.auth)
-
+    const { notes } = useSelector(state => state.journal)
     return (
         <Box
             component="nav"
@@ -29,19 +30,7 @@ export const SideBar = ({ drawerWidth = 240 }) => {
                 <Divider />
                 <List>
                     {
-                        ['Enero', 'Febrero', 'Marzo'].map(text => (
-                            <ListItem key={text} disablePadding>
-                                <ListItemButton>
-                                    <ListItemIcon>
-                                        <TurnedInNot />
-                                    </ListItemIcon>
-                                    <Grid container>
-                                        <ListItemText primary={text} />
-                                        <ListItemText secondary={`Edjflasjdflkasjfdlajsdfla`} />
-                                    </Grid>
-                                </ListItemButton>
-                            </ListItem>
-                        ))
+                        notes?.map(note => (<SidebarItem key={note.id} {...note} />))
                     }
                 </List>
 
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -3,13 +3,29 @@ import { IconButton, Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
 import { NothingSelectedView } from "../views/NothingSelectedView"
 import { NoteView } from "../views/NoteView"
+import { startNewNote } from "../../store/journal"
+import { useDispatch, useSelector } from "react-redux"
 
 export const JournalPage = () => {
+
+    const dispatch = useDispatch();
+
+    const { isSaving, active } = useSelector(state => state.journal)
+
+    const onClickNewNote = () => {
+        dispatch(startNewNote())
+    }
+
     return (
         <JournalLayout>
-            <NothingSelectedView />
-            {/* <NoteView/> */}
+            {
+                (!!active)
+                    ? <NoteView />
+                    : <NothingSelectedView />
+            }
             <IconButton
+                disabled={isSaving}
+                onClick={onClickNewNote}
                 size='large'
                 sx={{
                     color: 'white',
